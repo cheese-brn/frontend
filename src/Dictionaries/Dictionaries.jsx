6a06691897762c925c85d75bae8c3fcf4700e40e
widrelk <widrelk@gmail.com>
2021-12-03 00:26:58 +0700

    Initial branch commit

---
+++ src/Dictionaries/Dictionaries.jsx
@@ -0,0 +1,10 @@
+import React, {useEffect, useState} from "react";
+import {Paper, Grid} from '@mui/material';
+
+const Dictionaries = () => {
+	return(<>
+		Словарники
+	</>);
+}
+
+export default Dictionaries;
