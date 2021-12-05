d08fe8532e0c71eb0ea4bd81ce83f5598ec45548
widrelk <widrelk@gmail.com>
2021-12-05 20:18:57 +0700

    Fix "uncontrolled" problem and add connection to server

--- src/StainView/StainView.jsx
+++ src/StainView/StainView.jsx
@@ -4,116 +4,135 @@ import {Paper, Typography, Grid, TextField, Divider, Stack, Button} from "@mui/m
 import SimplePropertyInput from "./components/SimplePropertyInput";
 
 const StainView = () => {
-	const data = useParams();
-	const [model, setModel] = useState({});
+	const {stainID} = useParams();
+	const [model, setModel] = useState({
+					"id":0,
+					"rod":"",
+					"vid":"",
+					"annotation":"",
+					"exemplar":"",
+					"modification":"",
+					"obtainingMethod":"",
+					"origin":"",
+					"factParams":[],
+	});
 	const modelCopy = useRef(null);
 	const [isReadOnly, setIsReadOnly] = useState(true);
 	const [basicProps, setBasicProps] = useState([]);
 
 	useEffect(() => {
-		let stainData;
-		switch (data.stainID) {
-			case '0':
-				stainData = {
-					id: 0,
-					genus: 'Лактококус',
-					type: 'Лактис',
-					name: '977',
-					modification: 'С15',
-					obtainingMethod: 'Культивирование в отсутствии лактозы (Я.Р. Каган, И.Я. Сергеева)',
-					origin: 'Лактозоотрицательынй мутант штамма L. lactis 9772',
-					annotation: 'Может использоваться в исследованиях в качестве индикаторного штамма с селективным маркером\n\n\nasdadasdd',
-					lastEdit: 'Вчера, 20:00',
-					author: 'Иванов Иван Иванович',
-					actualProps: [
-						{
-							name: 'Наличие плазмид',
-							description: 'Ну есть плазмиды и есть. Или нету',
-							subProps: [
-								{
-									name: '',
-									value:['НД',]
-								}],
-						},
-						{
-							name: 'Маркеры',
-							description: 'Для доски',
-							subProps: [
-								{
-									name: '',
-									value:['Лизогенный; Lac Str',]
-								},
-							],
-						},
-						{
-							name: 'Фаготип',
-							description: 'Тип фагов',
-							subProps: [
-								{
-									name: '',
-									value:[
-										'Какие-то символы, кодировкой сделаем как надо',
-										'Но их будет много раз',
-										'так что храним в массиве',
-									]
-								},
-							],
-						},
-							{
-								name: 'Форма и расположение клеток',
-								description: 'Клеточная форма и расположение',
-								subProps: [
-									{
-										name: '',
-										value: [
-											'Кокки',
-											'Диплококки',
-										]
-									}
-								],
-							},
-							{
-								name: 'Форма и величина колоний',
-								description: 'Колониальная форма и величина',
-								subProps: [
-									{
-										name: 'Поверхностные',
-										value: 'Круглые каплевидные бла-бла-бла',
-									},
-									{
-										name: 'Глубинные',
-										value: 'дискообразные',
-									}
-								],
-							},
-						],
-						 // functionalProps: [
-						 // 	{
-						 // 		name: 'Кислотообразование в молоке',
-						 // 		description: 'Образование кислоты в среде молока',
-						 // 		subProps: [
-						 // 			{
-						 // 				name: 'С глюкозой',
-						 // 				value:
-						 // 			},
-						 // 			{
-						 // 				name: 'Без глюкозы',
-						 // 				value:
-						 // 			}
-						 // 		],
-						 // 	}
-						 // ],
-					};
-				break;
-			default:
-				return null;
-		}
-		setModel(stainData);
-		modelCopy.current = stainData;
-		}, [data.stainID]);
+		// switch (stainID) {
+		// 	case '0':
+		// 		stainData = {
+		// 			"id":1,
+		// 			"rod":"Lactococcus",
+		// 			"vid":"lactis",
+		// 			"annotation":"Может использоваться в исследованиях в качестве индикаторного штамма с селективным маркером",
+		// 			"exemplar":"977",
+		// 			"modification":"C15",
+		// 			"obtainingMethod":"Культивирование в отсутствии лактозы (Я.Р.Каган, И.Я.Сергеева)",
+		// 			"origin":"Лактозоотрицательный мутант штамма L. lactis 977.2",
+		// 			"factParams":[
+		// 				{
+		// 					"name":"Наличие плазмид",
+		// 					"description":"описание",
+		// 					"subProps":[
+		// 						{
+		// 							"name":"Наличие плазмид",
+		// 							"value":"Нд"
+		// 						}
+		// 					]
+		// 				},
+		// 				{
+		// 					"name":"Форма и расположение клеток",
+		// 					"description":"описание",
+		// 					"subProps":[
+		// 						{
+		// 							"name":"Форма и расположение клеток",
+		// 							"value":"Кокки, диплококки"
+		// 						}
+		// 					]
+		// 				},
+		// 				{
+		// 					"name":"Форма и величина колоний",
+		// 					"description":"описание",
+		// 					"subProps":[
+		// 						{
+		// 							"name":"Поверхностные",
+		// 							"value":"Круглые каплевидные с ровным краем диам. 2 мм"
+		// 						},
+		// 						{
+		// 							"name":"Глубинные",
+		// 							"value":"дискообразные"
+		// 						}
+		// 					]
+		// 				},
+		// 				{
+		// 					"name":"Аммиак из аргинина",
+		// 					"description":"описание",
+		// 					"subProps":[
+		// 						{
+		// 							"name":"Аммиак из аргинина",
+		// 							"value":"Образует"
+		// 						}
+		// 					]
+		// 				},
+		// 				{
+		// 					"name":"Утилизация цитрата",
+		// 					"description":"описание",
+		// 					"subProps":[
+		// 						{
+		// 							"name":"Утилизация цитрата",
+		// 							"value":"Не утилизирует"
+		// 						}
+		// 					]
+		// 				},
+		// 				{
+		// 					"name":"Ферментация углеводов",
+		// 					"description":"описание",
+		// 					"subProps":[
+		// 						{
+		// 							"name":"Ферментирует",
+		// 							"value":"глюкозу, галактозу, мальтозу, манит (слабо)"
+		// 						},
+		// 						{
+		// 							"name":"Не ферментирует",
+		// 							"value":"лактозу, сахарозу"
+		// 						}
+		// 					]
+		// 				},
+		// 				{
+		// 					"name":"Характер сгустка",
+		// 					"description":"описание",
+		// 					"subProps":[
+		// 						{
+		// 							"name":"Характер сгустка",
+		// 							"value":"-"
+		// 						}
+		// 					]
+		// 				},
+		// 				{
+		// 					"name":"Температура роста",
+		// 					"description":"описание",
+		// 					"subProps":[
+		// 						{
+		// 							"name":"Температура роста",
+		// 							"value":"Нд"
+		// 						}
+		// 					]
+		// 				}
+		// 			]
+		// 		};
+		// 		break;
+		// 	default:
+		// 		return null;
+		// }
+		fetch('/strain/10').then(response => response.json()).then(res => {console.log(res); setModel(res)})
+		modelCopy.current = model;
+		}, [stainID]);
 
 	useEffect(() => {
-		let props = model?.actualProps?.map((prop, key) => {
+		let props = model?.factParams?.map((prop, key) => {
 			return(<SimplePropertyInput
 				prop={prop}
 				propKey={key}
@@ -123,7 +142,7 @@ const StainView = () => {
 			/>);
 		});
 		setBasicProps(props);
-	}, [model?.actualProps, isReadOnly]);
+	}, [model?.factParams, isReadOnly]);
 
 	// TODO: Сделать нормально
 	const costilStyle = {
@@ -140,7 +159,6 @@ const StainView = () => {
 		setModel({...model, actualProps});
 	}
 
-	// TODO: fix 'A component is changing an uncontrolled input to be controlled' issue
 	// TODO: Разобраться с цветовой палитрой
 	return(
 			<Paper sx={{margin: '0 10px 0 10px', padding: '10px'}}>
@@ -154,27 +172,27 @@ const StainView = () => {
 								sx={costilStyle}
 								id='stain-view__genus-field'
 								label='Род'
-								name='genus'
+								name='rod'
 								inputProps={{readOnly: isReadOnly}}
-								value={model?.genus}
+								value={model?.rod}
 								onChange={ handleGenericChange }
 							/>
 							<TextField
 								sx={costilStyle}
 								id='stain-view__type-field'
 								label='Вид'
-								name='type'
+								name='vid'
 								inputProps={{readOnly: isReadOnly}}
-								value={model?.type}
+								value={model?.vid}
 								onChange={ handleGenericChange }
 							/>
 							<TextField
 								sx={costilStyle}
 								id='stain-view__name-field'
 								label='Наименование'
-								name='name'
+								name='exemplar'
 								inputProps={{readOnly: isReadOnly}}
-								value={model?.name}
+								value={model?.exemplar}
 								onChange={ handleGenericChange }
 							/>
 							<TextField
@@ -227,9 +245,9 @@ const StainView = () => {
 							<Typography variant='h6'>
 								{`Последнее редактирование:`}
 							</Typography>
-							<Typography>
-								{`${model.author}, ${model.lastEdit}`}
-							</Typography>
+							{/*<Typography>*/}
+							{/*	{`${model.author}, ${model.lastEdit}`}*/}
+							{/*</Typography>*/}
 							{isReadOnly &&
 							<Button
 								variant='contained'
