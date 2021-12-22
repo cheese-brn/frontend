14ae5026feeeeda853e5b678a2d83f2f468bcf85
widrelk <widrelk@gmail.com>
2021-12-23 00:20:46 +0700

    Add "add new property" functionality

--- src/StrainView/StrainView.jsx
+++ src/StrainView/StrainView.jsx
@@ -1,11 +1,12 @@
-import React, {useEffect, useState, useRef} from "react";
+import React, {useEffect, useState, useRef, useMemo} from "react";
 import {useParams, useNavigate} from "react-router-dom";
-import {Paper, Typography, Grid, TextField, Divider, Stack, Button, Select, MenuItem, FormControl, InputLabel} from "@mui/material";
+import {Paper, Typography, Grid, TextField, Divider, Stack, Button, Select, MenuItem, FormControl, InputLabel, Modal} from "@mui/material";
 import SimplePropertyInput from "./components/SimplePropertyInput";
 
 const StrainView = () => {
   const navigate = useNavigate();
   const {strainId} = useParams();
+
   const [model, setModel] = useState({
     "id":0,
     "rod":-1,
@@ -17,24 +18,26 @@ const StrainView = () => {
     "origin":"",
     "factParams":[],
   });
-
   const modelCopy = useRef(null);
-  const [isReadOnly, setIsReadOnly] = useState(true);
 
+  const [isReadOnly, setIsReadOnly] = useState(true);
+  const [addPropModalOpened, setAddPropModalOpened] = useState(false);
+
+  const [newPropId, setNewPropId] = useState(0);
   const [basicProps, setBasicProps] = useState([]);
   const [genusesList, setGenusesList] = useState(null);
   const [typesList, setTypesList] = useState(null);
-  useEffect(() => {
-    if(model.rod !== -1) {
-      fetch(`/vids/rods/${model.rod}`).then(response => response.json()).then(res => {
-        setTypesList(res);
-      });
-    }
-  }, [model.rod]);
+  const [propertiesList, setPropertiesList] = useState(null);
+
+  // TODO: Возможно, стоит инициализировать списки через useMemo.
+  //  Проблема в том, что не вызывается ре-рендер, в отличии от useState
 
   useEffect(() => {
     fetch('/rods').then(response => response.json()).then(res => {
       setGenusesList(res);
+    });
+    fetch('/properties').then(response => response.json()).then(res => {
+      setPropertiesList(res);
     });
 
     if (!strainId) {
@@ -44,6 +47,15 @@ const StrainView = () => {
     }
   }, []);
 
+  useEffect(() => {
+    if(model.rod !== -1) {
+      fetch(`/vids/rods/${model.rod}`).then(response => response.json()).then(res => {
+        setTypesList(res);
+      });
+    }
+  }, [model.rod]);
+
+  // TODO: правильнее перенести это в рендер
   useEffect(() => {
     const props = model?.factParams?.map((prop, key) => {
       return(<SimplePropertyInput
@@ -74,176 +86,220 @@ const StrainView = () => {
     setModel(newModel);
   };
 
+  const handleAddProperty = () => {
+    const newModel = JSON.parse(JSON.stringify(model));
+
+    fetch(`/properties/${newPropId}`).then(response => response.json()).then(propertyData => {
+      propertyData.subProps = [];
+      newModel.factParams.push(propertyData);
+      setModel(newModel);
+    });
+    setAddPropModalOpened(false);
+  }
+
   // TODO: Разобраться с цветовой палитрой
   // TODO: Разобраться с внешним видом полей, чтобы точно было всё как надо
   return(
-    <Paper sx={{margin: '0 10px 0 10px', padding: '10px'}}>
-      <Grid container spacing='5'>
-        <Grid container sm='6' md='7' lg='8' sx={{paddingRight: '15px', paddingLeft:'20px', }}>
-          <Stack orientation='vertical' width={'100%'}>
-            <Typography variant='h4'>
-								Паспорт штамма
-            </Typography>
-            <FormControl>
-              <InputLabel id='stain-view__genus-select-label'>Род</InputLabel>
-              <Select
-                sx={costilStyle}
-                labelId='stain-view__genus-select-label'
-                id='stain-view__genus-select'
-                value={model.rod}
-                name='rod'
-                onChange={handleCommonFieldChange}
-                inputProps={{readOnly: isReadOnly}}
-
-              >
-                {genusesList?.map(genus =>
-                  <MenuItem value={genus.id} key={genus.id}>{genus.name}</MenuItem>
-                )}
-              </Select>
-            </FormControl>
-
-            <FormControl>
-              <InputLabel id='stain-view__type-select-label'>Вид</InputLabel>
-              <Select
-                sx={costilStyle}
-                labelId='stain-view__type-select-label'
-                id='stain-view__type-select'
-                value={model.vid}
-                name='vid'
-                onChange={handleCommonFieldChange}
-                inputProps={{readOnly: isReadOnly || model.rod === -1}}
-              >
-                {typesList?.map(type =>
-                  <MenuItem value={type.id} key={type.id}>{type.name}</MenuItem>
-                )}
-              </Select>
-            </FormControl>
-
-            <TextField
-              sx={costilStyle}
-              id='stain-view__name-field'
-              label='Наименование'
-              name='exemplar'
-              inputProps={{readOnly: isReadOnly}}
-              value={model?.exemplar}
-              onChange={ handleCommonFieldChange }
-            />
-            <TextField
-              sx={costilStyle}
-              id='stain-view__modification-field'
-              label='Модификация'
-              name='modification'
-              inputProps={{readOnly: isReadOnly}}
-              value={model?.modification}
-              onChange={ handleCommonFieldChange }
-            />
-            <TextField
-              sx={costilStyle}
-              id='stain-view__obtaining-method-field'
-              label='Способ получения'
-              name='obtainingMethod'
-              inputProps={{readOnly: isReadOnly}}
-              value={model?.obtainingMethod}
-              onChange={ handleCommonFieldChange }
-              multiline
-            />
-            <TextField
-              sx={costilStyle}
-              id='stain-view__origin-field'
-              label='Происхождение'
-              name='origin'
-              inputProps={{readOnly: isReadOnly}}
-              value={model?.origin}
-              onChange={ handleCommonFieldChange }
-              multiline
-            />
-            <TextField
-              sx={costilStyle}
-              id='stain-view__annotation-field'
-              label='Аннотация'
-              name='annotation'
-              inputProps={{readOnly: isReadOnly}}
-              value={model?.annotation}
-              onChange={ handleCommonFieldChange }
-              multiline
-            />
-            <div style={{display: 'flex', justifyContent: 'space-between', marginBottom: '10px'}}>
-              <Typography variant='h5' align='left'>
-									Свойства штамма
+    <>
+      <Paper sx={{margin: '0 10px 0 10px', padding: '10px'}}>
+        <Grid container spacing='5'>
+          <Grid container sm='6' md='7' lg='8' sx={{paddingRight: '15px', paddingLeft:'20px', }}>
+            <Stack orientation='vertical' width={'100%'}>
+              <Typography variant='h4'>
+                  Паспорт штамма
               </Typography>
-              {!isReadOnly &&
-								<Button
-								  variant='contained'
-								  color='success'
-								  sx={{padding: '3px 8px 3px 8px'}}
-								>
-									Добавить свойство
-								</Button>
+              <FormControl>
+                <InputLabel id='stain-view__genus-select-label'>Род</InputLabel>
+                <Select
+                  sx={costilStyle}
+                  labelId='stain-view__genus-select-label'
+                  id='stain-view__genus-select'
+                  value={model.rod}
+                  name='rod'
+                  onChange={handleCommonFieldChange}
+                  inputProps={{readOnly: isReadOnly}}
+
+                >
+                  {genusesList?.map(genus =>
+                    <MenuItem value={genus.id} key={genus.id}>{genus.name}</MenuItem>
+                  )}
+                </Select>
+              </FormControl>
+
+              <FormControl>
+                <InputLabel id='stain-view__type-select-label'>Вид</InputLabel>
+                <Select
+                  sx={costilStyle}
+                  labelId='stain-view__type-select-label'
+                  id='stain-view__type-select'
+                  value={model.vid}
+                  name='vid'
+                  onChange={handleCommonFieldChange}
+                  inputProps={{readOnly: isReadOnly || model.rod === -1}}
+                >
+                  {typesList?.map(type =>
+                    <MenuItem value={type.id} key={type.id}>{type.name}</MenuItem>
+                  )}
+                </Select>
+              </FormControl>
+
+              <TextField
+                sx={costilStyle}
+                id='stain-view__name-field'
+                label='Наименование'
+                name='exemplar'
+                inputProps={{readOnly: isReadOnly}}
+                value={model?.exemplar}
+                onChange={ handleCommonFieldChange }
+              />
+              <TextField
+                sx={costilStyle}
+                id='stain-view__modification-field'
+                label='Модификация'
+                name='modification'
+                inputProps={{readOnly: isReadOnly}}
+                value={model?.modification}
+                onChange={ handleCommonFieldChange }
+              />
+              <TextField
+                sx={costilStyle}
+                id='stain-view__obtaining-method-field'
+                label='Способ получения'
+                name='obtainingMethod'
+                inputProps={{readOnly: isReadOnly}}
+                value={model?.obtainingMethod}
+                onChange={ handleCommonFieldChange }
+                multiline
+              />
+              <TextField
+                sx={costilStyle}
+                id='stain-view__origin-field'
+                label='Происхождение'
+                name='origin'
+                inputProps={{readOnly: isReadOnly}}
+                value={model?.origin}
+                onChange={ handleCommonFieldChange }
+                multiline
+              />
+              <TextField
+                sx={costilStyle}
+                id='stain-view__annotation-field'
+                label='Аннотация'
+                name='annotation'
+                inputProps={{readOnly: isReadOnly}}
+                value={model?.annotation}
+                onChange={ handleCommonFieldChange }
+                multiline
+              />
+              <div style={{display: 'flex', justifyContent: 'space-between', marginBottom: '10px'}}>
+                <Typography variant='h5' align='left'>
+                    Свойства штамма
+                </Typography>
+                {!isReadOnly &&
+                  <Button
+                    variant='contained'
+                    color='success'
+                    sx={{padding: '3px 8px 3px 8px'}}
+                    onClick={() => setAddPropModalOpened(true)}
+                  >
+                    Добавить свойство
+                  </Button>
+                }
+              </div>
+              {basicProps}
+            </Stack>
+          </Grid>
+          <Divider orientation="vertical" flexItem/>
+          <Grid item sx={{textAlign: 'left', marginLeft: '15px'}}>
+            <Stack>
+              <Typography variant='h6'>
+                {`Последнее редактирование:`}
+              </Typography>
+              {/*TODO: доделать, связано с CB-8*/}
+              {/*<Typography>*/}
+              {/*	{`${model.author}, ${model.lastEdit}`}*/}
+              {/*</Typography>*/}
+              {isReadOnly &&
+                <Button
+                  variant='contained'
+                  onClick={() => {
+                    setIsReadOnly(false);
+                  }}
+                >
+                  Редактировать
+                </Button>
               }
-            </div>
-            {basicProps}
-          </Stack>
-        </Grid>
-        <Divider orientation="vertical" flexItem/>
-        <Grid item sx={{textAlign: 'left', marginLeft: '15px'}}>
-          <Stack>
-            <Typography variant='h6'>
-              {`Последнее редактирование:`}
-            </Typography>
-            {/*TODO: доделать, связано с CB-8*/}
-            {/*<Typography>*/}
-            {/*	{`${model.author}, ${model.lastEdit}`}*/}
-            {/*</Typography>*/}
-            {isReadOnly &&
-							<Button
-							  variant='contained'
-							  onClick={() => {
-							    setIsReadOnly(false);
-							  }}
-							>
-								Редактировать
-							</Button>
-            }
-            {!isReadOnly && <>
-              <Button
-                variant='contained'
-                color='success'
-                sx={{marginTop: '20px'}}
-                onClick={() => {
-                  setIsReadOnly(true);
-                  // TODO: отправка модели на бэк
+              {!isReadOnly && <>
+                <Button
+                  variant='contained'
+                  color='success'
+                  sx={{marginTop: '20px'}}
+                  onClick={() => {
+                    setIsReadOnly(true);
+                    // TODO: отправка модели на бэк
 
-                }}>
-									Сохранить изменения
-              </Button>
+                  }}>
+                    Сохранить изменения
+                </Button>
+                <Button
+                  variant='contained'
+                  color='warning'
+                  sx={{marginTop: '20px'}}
+                  onClick={() => {
+                    setIsReadOnly(true);
+                    setModel({...modelCopy.current});
+                  }}>
+                    Отменить изменения
+                </Button>
+              </>}
               <Button
-                variant='contained'
-                color='warning'
+                variant='outlined'
+                color='error'
                 sx={{marginTop: '20px'}}
                 onClick={() => {
-                  setIsReadOnly(true);
-                  setModel({...modelCopy.current});
-                }}>
-									Отменить изменения
+                  // TODO: Модалка подтверждения удаления
+                  fetch(`/strain/delete/${model.id}`, {method: 'POST', headers: {'Content-Type': 'application/json'}});
+                  navigate(-1);
+                }}
+              >
+                  Удалить штамм
               </Button>
-            </>}
-            <Button
-              variant='outlined'
-              color='error'
-              sx={{marginTop: '20px'}}
-              onClick={() => {
-                // TODO: Модалка подтверждения удаления
-                fetch(`/strain/delete/${model.id}`, {method: 'POST', headers: {'Content-Type': 'application/json'}});
-                navigate(-1);
-              }}
+            </Stack>
+
+          </Grid>
+        </Grid>
+      </Paper>
+      <Modal
+        open={addPropModalOpened}
+        onClose={() => setAddPropModalOpened(false)}
+      >
+        <Paper sx={{width: '600px', maxHeight: '450px', margin: 'auto', padding: '20px'}}>
+          <Typography variant='h5'>
+            Добавление нового свойства
+          </Typography>
+          <Typography>Выберите свойство:</Typography>
+          <Select
+            sx={{width: '100%'}}
+            id='stain-view__new-property-type-select'
+            value={newPropId}
+            name='vid'
+            onChange={event => setNewPropId(event.target.value)}
+          >
+            {propertiesList?.map(property =>
+              <MenuItem value={property.id} key={property.id}>{property.name}</MenuItem>
+            )}
+          </Select>
+          <Button
+            variant='contained'
+            color='success'
+            sx={{padding: '3px 8px 3px 8px'}}
+            onClick={handleAddProperty}
             >
-								Удалить штамм
-            </Button>
-          </Stack>
-
-        </Grid>
-      </Grid>
-    </Paper>
+            Добавить
+          </Button>
+        </Paper>
+      </Modal>
+    </>
   );
 };
 
