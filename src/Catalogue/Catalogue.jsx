dec00977b381b60b9b1649be3af81dd6c6c03bb0
widrelk <widrelk@gmail.com>
2022-02-21 09:04:03 +0700

    Transfer commit

--- src/Catalogue/Catalogue.jsx
+++ src/Catalogue/Catalogue.jsx
@@ -1,7 +1,11 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Paper, Typography, Divider } from "@mui/material";
+import {Paper, Typography, Divider, Grid} from "@mui/material";
+import {TreeView, TreeItem,} from "@mui/lab"
 import { useNavigate } from "react-router-dom";
 import APP_ACTIONS from "../constants";
+
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 // eslint-disable-next-line react/prop-types
 const Catalogue = ({appDispatch}) => {
@@ -9,7 +13,20 @@ const Catalogue = ({appDispatch}) => {
   const genus = useRef(null);
   const navigate = useNavigate();
 
+  const [data, setData] = useState(null);
+
   useEffect(() => {
+    fetch(`/rods`)
+      .then(response => response.json())
+      .then(genuses => {
+
+        // TODO: Убрать этот костыль
+        let result = genuses.map(genus => ({
+          ...genus,
+          children: fetch(`/vids/rods/${genus.id}`).then(response => response.json)
+        }))
+        setData(result)
+      })
     handleReturnToGenus();
   }, []);
 
@@ -25,7 +42,6 @@ const Catalogue = ({appDispatch}) => {
           setDisplayData(json);
         });
     }
-
   };
 
   const handleReturnToGenus = () => {
@@ -37,7 +53,6 @@ const Catalogue = ({appDispatch}) => {
       .then(json => {
         setDisplayData(json);
       });
-
   };
   
   return(
@@ -48,22 +63,36 @@ const Catalogue = ({appDispatch}) => {
       >
           Каталог микроорганизмов
       </Typography>
-      <Typography align='left' variant='h5' onClick={handleReturnToGenus} sx={{marginLeft: '10px'}}>
-        {`Род${genus.current ? `: ${genus.current.name}, Вид:` : ''}`}
-      </Typography>
-      <Divider/>
-      <div style={{height: '80%' ,display: 'flex', flexDirection: 'column', overflowY: 'auto'}}>
-        {/*TODO: Сделать нормальный стиль компонента*/}
-        {displayData?.map((element, index) =>
-          <div
-            key={`catalogue-element-${index}`}
-            onClick={() => handleItemSelect(element)}
-            style={{height: '35px', textDecoration: 'underline', cursor: 'pointer'}}>
-            <Typography sx={{fontSize: '25px',}} align='left'>
-              {element.name}
-            </Typography>
-          </div>)}
-      </div>
+      <Grid container spacing="2">
+        <TreeView
+          defaultCollapseIcon={<ExpandMoreIcon />}
+          defaultExpandIcon={<ChevronRightIcon />}
+        >
+          {data?.map(genus => {
+            debugger
+              return (<TreeItem nodeId={`genus-${genus.id}`} label={genus.name}>
+                {genus.children?.map(type => <TreeItem nodeId={`type-${type.id}`} label={type.name}/>)}
+              </TreeItem>);
+            }
+          )}
+        </TreeView>
+      </Grid>
+      {/*<Typography align='left' variant='h5' onClick={handleReturnToGenus} sx={{marginLeft: '10px'}}>*/}
+      {/*  {`Род${genus.current ? `: ${genus.current.name}, Вид:` : ''}`}*/}
+      {/*</Typography>*/}
+      {/*<Divider/>*/}
+      {/*<div style={{height: '80%' ,display: 'flex', flexDirection: 'column', overflowY: 'auto'}}>*/}
+      {/*  /!*TODO: Сделать нормальный стиль компонента*!/*/}
+      {/*  {displayData?.map((element, index) =>*/}
+      {/*    <div*/}
+      {/*      key={`catalogue-element-${index}`}*/}
+      {/*      onClick={() => handleItemSelect(element)}*/}
+      {/*      style={{height: '35px', textDecoration: 'underline', cursor: 'pointer'}}>*/}
+      {/*      <Typography sx={{fontSize: '25px',}} align='left'>*/}
+      {/*        {element.name}*/}
+      {/*      </Typography>*/}
+      {/*    </div>)}*/}
+      {/*</div>*/}
     </Paper>
   );
 };
