3f5b6c048d8e4be58ed14037dea3096d89a28e6e
widrelk <widrelk@gmail.com>
2022-05-11 00:19:19 +0700

    Add first successful test

--- src/commons/components/PageHeader.jsx
+++ src/commons/components/PageHeader.jsx
@@ -7,10 +7,10 @@ import {useNavigate} from "react-router-dom";
 const PageHeader = ({header}) => {
   const navigate = useNavigate();
   return (<div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', marginBottom: '1em'}}>
-    <IconButton onClick={() => navigate(-1)}>
+    <IconButton onClick={() => navigate(-1)} id='page-header__go-back-button'>
       <ArrowBackIcon/>
     </IconButton>
-    <Typography variant='h4' align='left'>
+    <Typography variant='h4' align='left' id='page-header__header-text'>
       {header}
     </Typography>
   </div>)
